d0fce2c16e1d5dc0c3bf7e33d692e393287b5297
Selemani Lusukama Selestin <lusukamaselemani@gmail.com>
2026-08-04 19:51:29 +0200

    add example

---
+++ Learning-unit-1/exercise1.py
@@ -0,0 +1,41 @@
+# number1 = 5
+# number2 = 3
+
+
+# x = 2
+
+# x = x + 1 
+
+# x +=1
+
+# x -=2
+
+# x= 6
+
+# x /=2
+
+# x *=2
+
+
+# print("Incrememt : " , x)
+
+# print("Sum of number1 & 2 : " , number1 + number2)
+
+# print("Sum of number1 & 2 : " , number1 / number2)
+
+# print("Sum of number1 & 2 : " , number1 // number2)
+
+# print("Sum of number1 & 2 : " , 7 % 2)
+
+# print("Sum of number1 & 2 : " , 2 ** 3)
+
+
+
+a = 5 # initialized  
+b = 10
+a = a + b
+
+print(a)
+
+b = a % b
+print(b)
